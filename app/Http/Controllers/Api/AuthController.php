724bdc7a69ed9e2f096e70e00b17ddd4901df390
Kopchan <83940856+Kopchan@users.noreply.github.com>
2024-10-16 18:31:16 +0700

    Добавлена регистрация

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    /*
+     * Регистрация
+     */
+    public function signup(RegisterRequest $request)
+    {
+        $roleId = Role::firstOrCreate(['code' => 'user'])->id;
+
+        $user = User::create([
+            ...$request->validated(),
+            'role_id' => $roleId
+        ]);
+        $token = $user->remember_token = Str::random(100);
+        $user->save();
+        return response([
+            'token' => $token,
+            'data' => UserResource::make($user),
+        ], 201);
+    }
+
+
+    /*
+     * Авторизация
+     */
+
+
+    /*
+     * Выход
+     */
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::get('test', fn () => "hi");
+
+Route
+::controller(AuthController::class)
+->group(function () {
+    Route::post('signup', 'signup');
+    Route::post('login', 'login');
+});
